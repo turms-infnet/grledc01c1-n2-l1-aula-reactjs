4f006f3dc328ccfacaeabb04c6e479586b3746a7
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2024-11-08 22:08:49 -0300

    wip: Início da tela Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,22 @@
+import { Grid } from "../components";
+
 const Home: React.FC = () => {
-    return 'Home'
+    return  <Grid container={true}>
+                <Grid item={true} size={{xs: 12}}>
+                    <Grid container={true}>
+                        <Grid item={true} size={{xs: 4}}>1</Grid>
+                        <Grid item={true} size={{xs: 4}}>2</Grid>
+                        <Grid item={true} size={{xs: 4}}>3</Grid>
+                    </Grid>
+                </Grid>
+                <Grid item={true} size={{xs: 12}}>
+                    <Grid container={true}>
+                        <Grid item={true} size={{xs: 4}}>4</Grid>
+                        <Grid item={true} size={{xs: 4}}>5</Grid>
+                        <Grid item={true} size={{xs: 4}}>6</Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
 };
 
 export default Home;
